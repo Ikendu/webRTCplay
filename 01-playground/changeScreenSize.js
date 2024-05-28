7299480af489677f9c947a53c91894769b1c7589
Aniede Chibundu <ndubest56@gmail.com>
2024-05-28 14:25:23 +0100

    constrian explained

--- 01-playground/changeScreenSize.js
+++ 01-playground/changeScreenSize.js
@@ -3,10 +3,15 @@ const screenConstraint = navigator.mediaDevices.getSupportedConstraints();
 console.log(screenConstraint);
 
 const changeVideoSize = () => {
-  stream.getTracks().forEach((track) => {
-    const capabilities = track.getCapabilities();
-    console.log("CAPABILITIES", capabilities);
+  streams.getVideoTracks().forEach((track) => {
+    //track is a video track
+    // we can get its capabilities with track.getCapabilities
+    //or we can apply new constriants with get applyConstriants
   });
+  // stream.getTracks().forEach((track) => {
+  //   const capabilities = track.getCapabilities();
+  //   console.log("CAPABILITIES", capabilities);
+  // });
 };
 
 // changeVideoSize();
