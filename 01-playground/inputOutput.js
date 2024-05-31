f277958bd79c6d4f9884c7ad5916ab68b8f73cac
Aniede Chibundu <ndubest56@gmail.com>
2024-05-31 07:44:26 +0100

    get audio and video devices

--- 01-playground/inputOutput.js
+++ 01-playground/inputOutput.js
@@ -3,6 +3,7 @@ const getDevices = async () => {
     const devices = await navigator.mediaDevices.enumerateDevices();
     console.log(devices);
 
+    //get all the available devices
     devices.forEach((device) => {
       const option = document.createElement("option");
       option.value = device.deviceId;
@@ -20,6 +21,7 @@ const getDevices = async () => {
   }
 };
 
+//get the audio input devices
 const changeAudioInput = async (e) => {
   const deviceId = e.target.value;
   const newConstraint = {
@@ -28,26 +30,26 @@ const changeAudioInput = async (e) => {
   };
   stream = await navigator.mediaDevices.getUserMedia(newConstraint);
   console.log(stream);
-  const track = stream.getTracks();
-  console.log(track);
+  const track = stream.getAudioTracks();
+  console.log("Tracks", track);
 };
-
+//get the audio output devices
 const changeAudioOutput = async (e) => {
   const deviceId = e.target.value;
-  const newConstraint = {
-    audio: { deviceId: { exact: deviceId } },
-    video: true,
-  };
-  stream = await navigator.mediaDevices.getUserMedia(newConstraint);
-  console.log(stream);
-  const track = stream.getTracks();
+  await videoEl.setSinkId(deviceId);
 };
+
+//get the video input devices
 const changeVideoInput = async (e) => {
   const deviceId = e.target.value;
   const newConstraint = {
     audio: true,
     video: { deviceId: { exact: deviceId } },
   };
+  stream = await navigator.mediaDevices.getUserMedia(newConstraint);
+  console.log(stream);
+  const track = stream.getVideoTracks();
+  console.log("Tracks", track);
 };
 
 getDevices();
