8a31a00ca555d866c71de0c94f11172bbc087389
Aniede Chibundu <ndubest56@gmail.com>
2024-06-01 08:41:32 +0100

    webRTC local connection

--- signalingPeerConnection/script.js
+++ signalingPeerConnection/script.js
@@ -1,16 +1,48 @@
 const localVideoEl = document.querySelector("#local-video");
 const remoteVideo = document.querySelector("#remote-video");
 
-let localStream;
-let remoteStream;
-let peerConnection;
+let localStream; // for holding the local video streal
+let remoteStream; //  for holding the remote video stream
+let peerConnection; //  the connection that the two client use to talk
 
+//when a client initiates a call
 const videoCall = async (e) => {
-  const stream = navigator.mediaDevices.getUserMedia({
+  const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
   localVideoEl.srcObject = stream;
+
+  localStream = stream;
+
+  await createPeerConnection();
+  //after creating peer connection
+  //ctreate an offer
+  try {
+    console.log("creating offer....");
+    const offer = await peerConnection.createOffer();
+    console.log(offer);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const createPeerConnection = () => {
+  return new Promise(async (resolve, reject) => {
+    //RTCPeerConnection creates the connection
+    //We can pass config object which may contain stun server
+    //which will fetch us iceCandidate
+    peerConnection = await new RTCPeerConnection(peerConfiguration);
+    //getting and adding tracks to the peerConnection
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
+    peerConnection.addEventListener("icecandidate", (e) => {
+      console.log(".........peer connection found");
+      console.log(e);
+    });
+    resolve();
+  });
 };
 
 const callEl = document
