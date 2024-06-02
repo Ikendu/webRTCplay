f640109b28476ab6d3d56802e155be03b0dcb026
Aniede Chibundu <ndubest56@gmail.com>
2024-06-02 22:45:32 +0100

    all about https secure server

--- signalingPeerConnection/server.js
+++ signalingPeerConnection/server.js
@@ -1,6 +1,26 @@
+const fs = require("fs");
 const express = require("express");
+const https = require("https");
 const app = express();
+const socketio = require("socket.io");
 
 //use express to display the static html
 app.use(express.static(__dirname));
-app.listen(5000);
+
+// we need a key and a cert to run https
+// so we make them using mkcert (npm i -g mkcert)
+// > mkcert create-ca
+// > mkcert create-cert
+const cert = fs.readFileSync("cert.crt");
+const key = fs.readFileSync("cert.key");
+
+// we changed our express setup so we can use https
+// pass the key and cert to createServer securely with https
+const expressServer = https.createServer({ cert, key }, app);
+
+//create our socket.io server
+const io = socketio(expressServer);
+
+// io.on();
+
+expressServer.listen(5000, () => console.log("runing on port 5000"));
